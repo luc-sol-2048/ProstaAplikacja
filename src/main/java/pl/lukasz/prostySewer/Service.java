119881c6bb3891d4350b75163bc30d20217892db
lukasz137test <mufasa137@wp.pl>
2018-09-04 19:51:41 +0200

    Obsługa wielu wątków!!!!

--- src/main/java/pl/lukasz/prostySewer/Service.java
+++ src/main/java/pl/lukasz/prostySewer/Service.java
@@ -14,11 +14,11 @@ public class Service {
             .toMap(topic -> topic.topicName,topic -> topic);//dopisywanie do mapy nazyw Topicu i samego Topicu!!!
     }
 
-    Option<Topic> getTopics(String topicName){
+   synchronized Option<Topic> getTopics(String topicName){
         return topicMap.get(topicName);
     }
 
-    Option<Topic> addMessageToTopic(String topicName,Message message){
+     synchronized Option<Topic> addMessageToTopic(String topicName,Message message){
         Option<Topic> newTopic = getTopics(topicName).map(topic -> topic.addMesage(message));// wiadomość dopisana do topicu
         //nadpisywanie topicu do nowej mapy
         Option<Map<String, Topic>> newMap = newTopic.map(topic -> this.topicMap.put(topicName, topic));
