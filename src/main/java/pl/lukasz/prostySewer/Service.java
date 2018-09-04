1ca0f47033f43d76baee44b25efbef4b870733d0
lukasz137test <mufasa137@wp.pl>
2018-09-04 22:05:14 +0200

    Dodanie zapisu do pliku

--- src/main/java/pl/lukasz/prostySewer/Service.java
+++ src/main/java/pl/lukasz/prostySewer/Service.java
@@ -6,7 +6,9 @@ import io.vavr.control.Option;
 
 public class Service {
 
-    Map<String, Topic> topicMap;
+    private Map<String, Topic> topicMap;
+    private BoarMessageWriter writer = new BoarMessageWriter("mesages.jsons"); // inicjalizowanie pliku;
+
 
     Service(){
         this.topicMap=List.of("java", "vavr.io","spring")//lista z nazwami topiców
@@ -21,6 +23,7 @@ public class Service {
      synchronized Option<Topic> addMessageToTopic(String topicName,Message message){
         Option<Topic> newTopic = getTopics(topicName).map(topic -> topic.addMesage(message));// wiadomość dopisana do topicu
         //nadpisywanie topicu do nowej mapy
+         newTopic.forEach(topic -> writer.write(topic.topicName,message)); // zapisanie do pliku;
         Option<Map<String, Topic>> newMap = newTopic.map(topic -> this.topicMap.put(topicName, topic));
        //dopisywanie topicu do istniejącej mapy;
         newMap.forEach(topics->this.topicMap=topics);
