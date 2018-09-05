cb84cd11ab6c8a28cc0d3be4c5d79094b6bae361
lukasz137test <mufasa137@wp.pl>
2018-09-05 18:12:22 +0200

    Dodanie obsługi mongoDB zapis i odczyt danych z bazy danych

--- src/main/java/pl/lukasz/prostySewer/Service.java
+++ src/main/java/pl/lukasz/prostySewer/Service.java
@@ -1,28 +1,49 @@
 package pl.lukasz.prostySewer;
 
+import io.vavr.collection.HashMap;
 import io.vavr.collection.List;
 import io.vavr.collection.Map;
 import io.vavr.control.Option;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Service {
 
     public static final String MESAGES_JSONS = "mesages.jsons";
     private Map<String, Topic> topicMap;
-    private BoarMessageWriter writer = new BoarMessageWriter(MESAGES_JSONS); // inicjalizowanie pliku;
-
+    //private BoarMessageWriter writer = new BoarMessageWriter(MESAGES_JSONS); // inicjalizowanie pliku;
+    private final Repository repository; //repozytorium
 
     Service(){
-        this.topicMap=new BoardMessageReader().readAllTopics(MESAGES_JSONS);
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringInit.class);//stworzenie kontekstu dla spring
+        this.repository = context.getBean(Repository.class);//wyciągniety odpowiedni bean dla repository
+        this.topicMap = List.ofAll(repository.findAll())
+                        .foldLeft(HashMap.<String,Topic>empty(),
+                                (existingMap,newElement)->
+                                 existingMap.put(newElement.topic,existingMap.get(newElement.topic)
+                                         .getOrElse(Topic.create(newElement.topic))
+                                         .addMesage(newElement.message)));
+        ensureTopic("java");
+        ensureTopic("vavr.io");
+        ensureTopic("ogolny");
+
+        //this.topicMap=new BoardMessageReader().readAllTopics(MESAGES_JSONS);
     }
 
-   synchronized Option<Topic> getTopics(String topicName){
+    private void ensureTopic(String java) {
+        this.topicMap = topicMap.computeIfAbsent(java, key -> Topic.create(key))._2;
+    }
+
+    synchronized Option<Topic> getTopics(String topicName){
         return topicMap.get(topicName);
     }
 
      synchronized Option<Topic> addMessageToTopic(String topicName,Message message){
         Option<Topic> newTopic = getTopics(topicName).map(topic -> topic.addMesage(message));// wiadomość dopisana do topicu
         //nadpisywanie topicu do nowej mapy
-         newTopic.forEach(topic -> writer.write(topic.topicName,message)); // zapisanie do pliku;
+
+         newTopic.forEach(topic -> repository.save(new BoardMessage(message,topicName))); //zapisywanie do repozytorium i bazy
+         //newTopic.forEach(topic -> writer.write(topic.topicName,message)); // zapisanie do pliku;
         Option<Map<String, Topic>> newMap = newTopic.map(topic -> this.topicMap.put(topicName, topic));
        //dopisywanie topicu do istniejącej mapy;
         newMap.forEach(topics->this.topicMap=topics);
