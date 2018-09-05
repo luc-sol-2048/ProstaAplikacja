3a6da7294624e5fa964d66ce57680d3ea05e3a26
lukasz137test <mufasa137@wp.pl>
2018-09-05 19:44:15 +0200

    Dodanie czasu do zapisu wiadomości

--- src/main/java/pl/lukasz/prostySewer/Message.java
+++ src/main/java/pl/lukasz/prostySewer/Message.java
@@ -3,13 +3,21 @@ package pl.lukasz.prostySewer;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Message {
 
     public final String content;
     public final String author;
+    @JsonProperty("time")
+    public final String time;
     @JsonCreator
     public Message(@JsonProperty("content") String content,@JsonProperty("author") String author) {
         this.content = content;
         this.author = author;
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss") ;
+        LocalDateTime local = LocalDateTime.now();
+        this.time = dateTimeFormatter.format(local);
     }
 }
